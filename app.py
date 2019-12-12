7fc428b4382969bfb635a361a6a6b16e599726dc
Cao Mai <caomai@rocketmail.com>
2019-12-12 00:31:45 -0800

    [update] pages to have proper title

--- app.py
+++ app.py
@@ -10,33 +10,33 @@ def index():
 @app.route('/product_page')
 def about():
     """Return homepage."""
-    return render_template("product_page.html")
+    return render_template("product_page.html", title="Product Page")
 
 @app.route('/sign_up')
 def sign_up():
     """Return homepage."""
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", title="Sign Up Now")
 
 
 @app.route('/login')
 def login():
     """Return homepage."""
-    return render_template("login.html")
+    return render_template("login.html", title="Login")
 
 @app.route('/about')
 def about_page():
     """Return homepage."""
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 @app.route('/featured')
 def featured():
     """Return homepage."""
-    return render_template("featured.html")
+    return render_template("featured.html", title="Featured Listings")
 
-@app.route('/search')
-def search():
-    """Return homepage."""
-    return render_template("search.html")
+# @app.route('/search')
+# def search():
+#     """Return homepage."""
+#     return render_template("search.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
